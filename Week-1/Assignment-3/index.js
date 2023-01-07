a044ba0360bb8a49a9a5719164d9a9320d11048c
Fock Space <a86gj3sp4@gmail.com>
2023-01-07 18:28:23 +0800

    refactor the function

--- Week-1/Assignment-3/index.js
+++ Week-1/Assignment-3/index.js
@@ -1,20 +1,25 @@
 function countAandB(input) {
-  let count = 0;
-  for (char of input) {
-    if (char === "a" || char === "b") count++;
+  let count_a = 0,
+    count_b = 0;
+  for (c of input) {
+    if (c === "a") count_a++;
+    else if (c === "b") count_b++;
   }
-  return count;
+  const text =
+    count_a + count_b > 0
+      ? `${count_a + count_b} (${count_a} 'a' ${
+          count_a > 1 ? "letters" : "letter"
+        } and ${count_b} 'b' ${count_b > 1 ? "letters" : "letter"})`
+      : 0;
+  return text;
 }
+
 function toNumber(input) {
-    const array = [];
-    for (char of input) {
-        array.push(char.charCodeAt(0) - 'a'.charCodeAt(0) + 1);
-    }
-    return array;
+  return input.map((i) => i.charCodeAt(0) - "a".charCodeAt(0) + 1);
 }
 
 let input1 = ["a", "b", "c", "a", "c", "a", "c"];
-console.log(countAandB(input1)); // should print 4 (3 ‘a’ lettersand 1 ‘b’ letter)
+console.log(countAandB(input1)); // should print 4 (3 ‘a’ letters and 1 ‘b’ letter)
 console.log(toNumber(input1)); // should print [1, 2, 3, 1, 3, 1,3]
 
 let input2 = ["e", "d", "c", "d", "e"];
