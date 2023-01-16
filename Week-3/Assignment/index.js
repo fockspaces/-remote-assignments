d27a22c81d20d34f4c6b5e7f0708b24137213f3e
Fock Space <a86gj3sp4@gmail.com>
2023-01-16 20:50:25 +0800

    finish Assignment 2: Build Backend API for Front-End

--- Week-3/Assignment/index.js
+++ Week-3/Assignment/index.js
@@ -6,6 +6,21 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello, My Server!</h1>");
 });
 
+app.get("/data", (req, res) => {
+  res.send("<h1>Lack of Parameter</h1>");
+});
+
+app.get("/data/:number", (req, res) => {
+  const num = parseInt(req.params.number);
+
+  // if integer => sum of 1 + 2 + ... + n else error message
+  const renderedText = Number.isInteger(num)
+    ? `<h2>The sum of the number from (1 to ${num}) is : ${(num * (num + 1)) / 2}</h2>`
+    : "<h2>Wrong Parameter (not an integer)</h2>";
+
+  res.send(renderedText);
+});
+
 app.listen(port, () => {
-    console.log(`listening on ${port}`);
-})
+  console.log(`listening on ${port}`);
+});
