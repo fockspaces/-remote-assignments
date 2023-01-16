158c3f44328014ba7341c819abe34792f891a58b
Fock Space <a86gj3sp4@gmail.com>
2023-01-17 00:41:13 +0800

    refactor axios method in a-2

--- Week-3/Assignment/public/sum.js
+++ Week-3/Assignment/public/sum.js
@@ -1,10 +1,9 @@
 // axios method
-const fetchData = async (number) => {
-  const res = await axios.get(`http://localhost:3000/data?number=${number}`);
-  return res.data;
-};
-
-const setText = async (number) => {
+const setTextAxios = async (number) => {
+  const fetchData = async (number) => {
+    const res = await axios.get(`http://localhost:3000/data?number=${number}`);
+    return res.data;
+  };
   const data = await fetchData(number);
   document.querySelector(".message").innerHTML = data;
 };
@@ -27,6 +26,6 @@ const inputText = document.getElementById("inputText");
 
 inputForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  // setText(inputText.value);
-  setTextHTTP(inputText.value);
+  setTextAxios(inputText.value);
+  // setTextHTTP(inputText.value);
 });
