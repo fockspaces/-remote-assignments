778d0194468d4f945370b08e87b5428e53bbc26f
Fock Space <a86gj3sp4@gmail.com>
2023-01-17 18:58:02 +0800

    exp route

--- Week-3/Assignment/assignment-5.js
+++ Week-3/Assignment/assignment-5.js
@@ -39,6 +39,41 @@ function twoSum_3(nums, target) {
 
 const nums = [2, 7, 15, 11];
 const target = 9;
-console.log(twoSum_1(nums, target));
-console.log(twoSum_2(nums, target));
-console.log(twoSum_3(nums, target));
+// console.log(twoSum_1(nums, target));
+// console.log(twoSum_2(nums, target));
+// console.log(twoSum_3(nums, target));
+
+// testing
+const N = 100000;
+const data = {};
+
+const test = (func, times) => {
+  for (i = 0; i < times; ++i) {
+    func(nums, target);
+  }
+};
+
+const measure = (func, times) => {
+  const startTime = performance.now();
+  test(func, times);
+  const endTime = performance.now();
+  console.log(
+    `Call to ${func.name} ${times} times took ${
+      Math.round((endTime - startTime) * 1000) / 1000
+    } milliseconds`
+  );
+  // if (!data[`${times}`]) data[`${times}`] = [];
+  // data[`${times}`].push(Math.round((endTime - startTime) * 1000) / 1000);
+};
+
+// for (let i = 100; i < N; i += 1000) {
+//   measure(twoSum_1, i);
+//   measure(twoSum_2, i);
+//   measure(twoSum_3, i);
+// }
+
+// console.log(data);
+
+measure(twoSum_1, 100000);
+measure(twoSum_2, 100000);
+measure(twoSum_3, 100000);
