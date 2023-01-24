fa24cd74b213603b122ab7d2bdb2663697654a54
Fock Space <a86gj3sp4@gmail.com>
2023-01-24 10:45:43 +0800

    rearrange the code for a-1

--- Week-4/Assignment-1/index.js
+++ Week-4/Assignment-1/index.js
@@ -1,21 +1,11 @@
 // A. Complete the function below to show a delayed result in the console.
-
 function delayedResult(n1, n2, delayTime, callback) {
   setTimeout(() => {
     callback(n1 + n2);
   }, delayTime);
 }
-// delayedResult(4, 5, 3000, function (result) {
-//   console.log(result);
-// }); 
-// // 9 (4+5) will be shown in the console after 3 seconds
-// delayedResult(-5, 10, 2000, function (result) {
-//   console.log(result);
-// }); 
-// 5 (-5+10) will be shown in the console after 2 seconds
 
 // B. To implement delayedResult again using promise this time. It should look like:
-
 function delayedResultPromise(n1, n2, delayTime) {
   return new Promise(function (resolve, reject) {
     setTimeout(function () {
@@ -25,15 +15,27 @@ function delayedResultPromise(n1, n2, delayTime) {
   });
 }
 
+// C. To implement it using async/await this time.
+async function main() {
+  const res = await delayedResultPromise(4, 5, 3000);
+  console.log(res);
+}
+
+// #1
+// delayedResult(4, 5, 3000, function (result) {
+//   console.log(result);
+// });
+// // 9 (4+5) will be shown in the console after 3 seconds
+// delayedResult(-5, 10, 2000, function (result) {
+//   console.log(result);
+// });
+// 5 (-5+10) will be shown in the console after 2 seconds
+
+// #2
 // delayedResultPromise(4, 5, 3000)
 //   .then(console.log)
 //   .catch((e) => console.log(e.message));
 // 9 (4+5) will be shown in the console after 3 seconds
 
-// C. To implement it using async/await this time.
-
-async function main() {
-  const res = await delayedResultPromise(4, 5, 3000);
-  console.log(res);
-}
+// #3
 // main(); // result will be shown in the console after <delayTime>seconds
