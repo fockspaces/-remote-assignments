4dfcf1ef3fdddb5d56cdb9d2ade267ab0ea5997d
Fock Space <a86gj3sp4@gmail.com>
2023-01-26 18:39:39 +0800

    setup template engine

--- Week-4/Assignment-3/app.js
+++ Week-4/Assignment-3/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const user = require("./routes/users");
+const path = require("path");
 
 const app = express();
 const port = 3000;
@@ -8,8 +9,11 @@ app.use(express.json()); // to support JSON-encoded bodies
 app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
 
 // routers
-app.use(express.static(__dirname + "/views"));
 app.use("/users", user);
+
+// template engine
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 app.listen(port, () => {
   console.log(`listening on ${port}`);
--- Week-4/Assignment-3/routes/users.js
+++ Week-4/Assignment-3/routes/users.js
@@ -2,8 +2,9 @@ const express = require("express");
 const user = express.Router();
 const { auth, login, signup } = require("../controllers/users");
 
+
 user.get("/", (req, res) => {
-  res.redirect("homepage.html");
+  res.render("homepage");
 });
 
 user.post("/signup", signup);
